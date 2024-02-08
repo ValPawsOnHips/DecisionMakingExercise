0adc46ab0117da3d0efd13724e5a676a32d14d94
ValPawsOnHips <hypnozeta12@gmail.com>
2024-02-08 10:43:27 -0600

    Finished

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ if (randomNum != 3 && randomNum != 5) {
 if (randomNum == 2 || randomNum == 4) {
   console.log("Equal to 2 OR equal to 4");
 }
+
+// BONUS
+console.log(randomNum);
+randomNum >= 4
+  ? console.log("Greater than or equal to 4")
+  : console.log("Less than 4");
+
+switch (randomNum) {
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    break;
+}
